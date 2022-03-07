878bd8e97a87908cc91e43a1939dad1d7b652fba
Gregory Cook <gcook.mobile@gmail.com>
2022-03-06 20:52:47 -0800

    Updated configured wallet. Fixed parameters for compoundRewards(). Hard coded fixed, low value for deposit in compound rewards to allow more testing. Replaced some in code abi values with ones pulled from configuration js files.

--- autocompound_curve_pools/avalanche_aave_pool/autocompound.js
+++ autocompound_curve_pools/avalanche_aave_pool/autocompound.js
@@ -6,7 +6,7 @@ const { wallets } = require('./wallets')
 const { tokens } = require('./tokens')
 
 const chain = chains.avalanche
-const wallet = wallets.avalanche.test_account_1
+const wallet = wallets.avalanche.primary
 const web3 = new Web3(chain.rpc_url)
 
 const aave = require('./aave.js')
@@ -39,7 +39,10 @@ async function autoClaimAndCompound(){
     claimed = await attemptRewardClaim(rewardTokens, tokenRewards)
     // compounded = await compoundRewards(rewardTokens, tokenRewards)
     // wavax = '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7'
-    compounded = await compoundRewards(tokens.avalanche.wavax)
+    compounded = await compoundRewards(tokens.avalanche.wavax.address)
+    console.log(`Compounded: ${compounded}`)
+    console.log('-----------------------')
+    console.log()
 }
 
 async function getRewardTokens(smartContract){
@@ -79,7 +82,7 @@ async function attemptRewardClaim(rewardTokens, tokenRewards){
         if(web3.utils.toBN(tokenRewards[wavaxIndex]).gte(web3.utils.toBN(web3.utils.toWei('0.1')))){
             console.log(`There's enough rewards to claim, let's try it.`)
             claimed = await claimRewards()
-        }else console.log(`Not enough rewards ${web3.utils.fromWei(tokenRewards[wavaxIndex])} to claim for ${rewardTokens[wavaxIndex]}`)
+        } else console.log(`Not enough rewards ${web3.utils.fromWei(tokenRewards[wavaxIndex])} to claim for ${rewardTokens[wavaxIndex]}`)
     }catch(e){
         console.error(`The following error occurred whilst attempting the reward claim: ${e}`)
     }
@@ -109,9 +112,11 @@ async function claimRewards(){
 async function compoundRewards(tokenAddress){
     try{
         tokenBalance = await getTokenBalance(tokenAddress)
-        if(web3.utils.toBN(tokenBalance).gte(web3.utils.toBN(web3.utils.toWei('0.1')))){
+        bTokenBalance = web3.utils.toBN(tokenBalance)
+        bThreshold = web3.utils.toBN(web3.utils.toWei('0.2'))
+        if(bTokenBalance.gte(bThreshold)){
             console.log(`Let's compound token Rewards ${web3.utils.fromWei(tokenBalance)}`)
-            success = await aave.deposit(tokenAddress, tokenBalance, onBehalfOf)
+            success = await aave.deposit(tokenAddress, web3.utils.toWei('0.1'), onBehalfOf)
             if(success) console.log(`Rewards compounded, yay!!! :)`)
             return success
         }
@@ -134,13 +139,11 @@ function getWavaxRewards(rewardTokens, tokenRewards){
     return 0
 }
 
-async function getTokenBalance(token){
+async function getTokenBalance(tokenAddress){
     balance = 0
     try{
-        erc20abi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function","signature":"0x06fdde03"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0x095ea7b3"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function","signature":"0x18160ddd"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0x23b872dd"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function","signature":"0x313ce567"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0x39509351"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0x40c10f19"},{"constant":false,"inputs":[{"name":"value","type":"uint256"}],"name":"burn","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0x42966c68"},{"constant":true,"inputs":[{"name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function","signature":"0x70a08231"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"value","type":"uint256"}],"name":"burnFrom","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0x79cc6790"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function","signature":"0x95d89b41"},{"constant":false,"inputs":[{"name":"account","type":"address"}],"name":"addMinter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0x983b2d56"},{"constant":false,"inputs":[],"name":"renounceMinter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0x98650275"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0xa457c2d7"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function","signature":"0xa9059cbb"},{"constant":true,"inputs":[{"name":"account","type":"address"}],"name":"isMinter","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function","signature":"0xaa271e1a"},{"constant":true,"inputs":[{"name":"owner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function","signature":"0xdd62ed3e"},{"inputs":[{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint8"}],"payable":false,"stateMutability":"nonpayable","type":"constructor","signature":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"MinterAdded","type":"event","signature":"0x6ae172837ea30b801fbfcdd4108aa1d5bf8ff775444fd70256b44e6bf3dfc3f6"},{"anonymous":false,"inputs":[{"indexed":true,"name":"account","type":"address"}],"name":"MinterRemoved","type":"event","signature":"0xe94479a9f7e1952cc78f2d6baab678adc1b772d936c6583def489e524cb66692"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event","signature":"0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event","signature":"0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925"}]
-        tokenSC = new web3.eth.Contract(erc20abi, token)
+        tokenSC = new web3.eth.Contract(tokens.avalanche.wavax.abi, tokens.avalanche.wavax.address)
         balance = await tokenSC.methods.balanceOf(onBehalfOf).call()
-        return balance
     }catch(e){
         console.error(`getTokenBalance: Error ${e}`)
     }
